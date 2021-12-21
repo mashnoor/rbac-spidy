1ef9837cc07fc77a75bb62c9ebb81288fd13dfab
Nowfel Mashnoor <nowfel.mashnoor@upaybd.com>
2021-12-21 12:05:57 +0600

    Done

---
+++ kubehelpers/roles_generator.go
@@ -0,0 +1,43 @@
+package kubehelpers
+
+import "fmt"
+
+func GenerateRole(namespace, roleName string) string {
+
+	role := fmt.Sprintf(`
+kind: Role
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  namespace: %s
+  name: %s
+rules:
+- apiGroups: [""]
+  resources: ["deployments", "pods"]
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+
+`, namespace, roleName)
+
+	return role
+
+}
+
+func GenerateRoleBinding(namespace, roleBindingName, userName, roleName string) string {
+	roleBinding := fmt.Sprintf(`
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  name: %s
+  namespace: %s
+subjects:
+  - kind: User
+    name: %s
+    apiGroup: ""
+roleRef:
+  kind: Role
+  name: %s
+  apiGroup: ""
+`, roleBindingName, namespace, userName, roleName)
+
+	return roleBinding
+
+}
